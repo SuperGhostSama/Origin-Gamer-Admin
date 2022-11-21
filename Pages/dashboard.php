ad87fe7c4a0f38f1c5e0faf2c9613b5f0fb8463d
Salah <salaheddinehanaoui@gmail.com>
2022-11-21 17:22:51 +0100

    made a new session file where i used session start

--- Pages/dashboard.php
+++ Pages/dashboard.php
@@ -1,6 +1,7 @@
 <?php
 // Initialize the session
-session_start();
+// session_start();
+include('session.php');
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
