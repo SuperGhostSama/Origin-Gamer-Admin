7d8f2e1b5fca79533a732f6bc9d1cf6bc5bf5d20
Salah <salaheddinehanaoui@gmail.com>
2022-11-21 12:08:27 +0100

    made it so that when we click outside of the modal it doesnt close

--- Pages/products.php
+++ Pages/products.php
@@ -136,7 +136,7 @@
 
   
 
-	<div class="modal fade" id="modal-product">
+	<div class="modal fade" id="modal-product" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true" >
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<form action="" method="POST" id="form">
