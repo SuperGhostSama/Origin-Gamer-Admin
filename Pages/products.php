eb628519074ccd1fdf9ec3d54de7cbb1f0997145
Salah <salaheddinehanaoui@gmail.com>
2022-11-22 11:54:20 +0100

    added an alert using bootstrap that shows saved/updated/deleted successefully

--- Pages/products.php
+++ Pages/products.php
@@ -76,12 +76,17 @@
           </div>
         </nav>
   </header>  
-<?php
-  if(isset($_SESSION['message'])){
-      echo $_SESSION['message'];
-      unset($_SESSION['message']);
-  }
-?>
+            <!-- MESSAGE NOTIFICATION -->
+          <?php
+            if(isset($_SESSION['message'])){?>
+
+            <div class="alert alert-primary alert-dismissible fade show" role="alert">
+              <strong><?= $_SESSION['message'];?></strong>
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+
+          <?php unset($_SESSION['message']);}?>
+
   <section>
     <!-- PRODUCTS TABLE -->
     <div class="tableContainer">
