04500acd36ffc0927565b462677809583cbb12a7
Salah <salaheddinehanaoui@gmail.com>
2022-11-22 15:42:15 +0100

    cleaned the code

--- Pages/products.php
+++ Pages/products.php
@@ -41,7 +41,7 @@
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <title>Products</title>
-    <script  src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script> <!--defer makes html and js work at the sametime -->
+    <script  src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script> 
     <script  src="//cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
     
 </head>
@@ -212,10 +212,6 @@
   <?php }
     ?>
   </script>
-  <!-- used datatables.net to get the search -->
-  
-  
-
   <script src="https://parsleyjs.org/dist/parsley.min.js"></script>
   <script src="../assets/js/script.js" ></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
